b1cab90fc5176c7678dab9350cfa848a45ac49d5
fabiosvd <fabiosalvador85@yahoo.com.br>
2021-09-06 20:08:11 -0300

    started  assignments

---
+++ assignments.js
@@ -0,0 +1,22 @@
+// LECTURE: Values and Variables
+
+let country = 'Brazil';
+const continent = 'South America';
+let population = '211 Millions';
+
+// console.log(country);
+// console.log(continent);
+// console.log(population);
+
+// LECTURE: Data Types
+
+const isIsland = false;
+let language;
+
+console.log(country, continent, population, isIsland);
+
+// LECTURE: let, const and var
+language = 'Portuguese';
+console.log(language);
+
+// LECTURE: Basic Operators
